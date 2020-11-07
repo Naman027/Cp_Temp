1e369874486f72e8a237e5bc98daad749ceaacce
Naman Goyal <60535864+NamanG27@users.noreply.github.com>
2020-11-07 13:29:25 +0530

    Sieve Code added

---
+++ Number Theory/sieve.cpp
@@ -0,0 +1,25 @@
+void sieveprime(){
+    memset(prime,true,sizeof(prime));
+    for(int p=2;p*p<maxN;p++){
+        if(prime[p]){
+            for(int i=2*p;i<maxN;i+=p){
+                prime[i]=false;
+            }
+        }
+    }
+}
+ 
+void sieve(){ 
+    spf[1] = 1; 
+    for (ll i=2; i<maxN; i++) 
+        spf[i] = i; 
+    for (ll i=4; i<maxN; i+=2) 
+        spf[i] = 2; 
+    for (ll i=3; i*i<maxN; i++){  
+        if (spf[i] == i){ 
+            for (ll j=i*i; j<maxN; j+=i) 
+                if (spf[j]==j) 
+                    spf[j] = i; 
+        } 
+    } 
+}
