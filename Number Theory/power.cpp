6e34da547d692078fcec813af4564fa1a3e8b9c6
Naman Goyal <60535864+NamanGoyal07@users.noreply.github.com>
2020-12-23 17:55:30 +0530

    power.cpp

---
+++ Number Theory/power.cpp
@@ -0,0 +1,25 @@
+
+ll POW(ll base,ll exp){
+    ll result = 1;
+    while (exp > 0){
+        if (exp & 1)
+            result = ((ll)result * base);
+        base = ((ll)base * base);
+        exp >>= 1;
+    }
+    return result;
+}
+-------------------------------------------------------------------------------------------------
+ll mod = 1e9+7; 
+ll mpow(ll base, ll exp){
+    base %= mod;
+    ll result = 1;
+    while (exp > 0){
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+-------------------------------------------------------------------------------------------------
