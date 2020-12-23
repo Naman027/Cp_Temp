51ff99179a80a94367c4a1ba997e6d9a1a654139
Naman Goyal <60535864+NamanGoyal07@users.noreply.github.com>
2020-12-23 18:06:55 +0530

    Div_count.cpp

---
+++ Number Theory/Div_count.cpp
@@ -0,0 +1,22 @@
+ll count_divisors(ll num){
+    ll count = 0;
+    ll pp = 1;
+    while (num % 2 == 0){
+        count++;
+        num /= 2;
+    }
+    pp *= (count + 1);
+    for (ll i = 3; i <= sqrt(num); i += 2){
+        count = 0;
+        while (num % i == 0)
+        {
+            count++;
+            num /= i;
+        }
+        pp *= (count + 1);
+    }
+    if (num > 2){
+        pp *= 2;
+    }
+    return pp;
+}
