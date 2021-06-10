cc802ff00177debb70b7f4395dc3317410a7eeb9
adriraj2000 <50839399+adriraj2000@users.noreply.github.com>
2021-06-10 18:27:04 +0530

    Update fenwick.cpp

--- Trees/fenwick.cpp
+++ Trees/fenwick.cpp
@@ -1,18 +1,52 @@
-struct BIT{
-    vector<ll> bit;
-    BIT(){
-        bit.assign(maxN, 0LL);
-    }
-    void update(int ind, ll val){
-        for(; ind < maxN; ind += (ind & (-ind))){
-            bit[ind] += val;
+class NumArray {
+public:
+    vector<int> a;
+    vector<int> bit;
+    
+    NumArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int n = nums.size();
+        
+        a = nums;
+        bit = vector<int> (n+1, 0);
+        
+        for(int i=0;i<n;i++){
+            update_bit(i, a[i]);
         }
     }
-    ll query(int ind){
-        ll ret = 0LL;
-        for(; ind; ind -= (ind & (-ind))){
-            ret += bit[ind];
+    
+    void update(int i, int val) {
+        update_bit(i, val - a[i]);
+        a[i] = val;
+    }
+    
+    int sumRange(int i, int j) {
+        return getsum(j) - getsum(i-1);
+    }
+    
+    void update_bit(int idx, int val){
+        idx++;
+        
+        int n = a.size();
+        
+        while(idx <= n){
+            bit[idx] += val;
+            
+            idx += (idx & -idx);
         }
-        return ret;
+    }
+    
+    int getsum(int idx){
+        idx++;
+        
+        int res = 0;
+        while(idx > 0){
+            res += bit[idx];
+            
+            idx -= (idx & -idx);
+        }
+        return res;
     }
 };
