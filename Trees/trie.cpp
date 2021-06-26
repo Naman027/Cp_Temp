9fbd422a0250aeac515e24a1d5418784e111e2a4
NamanGoyal07 <naman18@iitg.ac.in>
2021-06-26 16:57:48 +0530

    Strings Trie

--- Trees/trie.cpp
+++ Trees/trie.cpp
@@ -12,7 +12,7 @@ int const SIZE = 26;
 struct node{
     char c;
     int count; // count of words with this letter
-    bool endOfWord;
+    bool endOfWord; // int can also be used for endOfWord to denote no of strings ending 
     node* ar[SIZE];
 };
 
@@ -48,6 +48,18 @@ bool search(node* root,string s){
         tempRoot = tempRoot->ar[index];
     }
     return tempRoot->endOfWord;
+}
+
+bool deleteNode(node* root,string s){
+    node* tempRoot = root;
+    for(int i=0;i<s.size();i++){
+        int index = s[i]-'a';
+        if(tempRoot->ar[index]==nullptr) return false;
+        tempRoot = tempRoot->ar[index];
+    }
+    if(tempRoot->endOfWord==false) return false;
+    tempRoot->endOfWord = false;
+    return true;
 }
 
 int32_t main(){
