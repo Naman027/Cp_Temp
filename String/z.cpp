e2143770bd8752f45e93da7f41bf721598ebc541
NamanGoyal07 <naman18@iitg.ac.in>
2021-06-26 12:34:27 +0530

    Strings

--- String/z.cpp
+++ String/z.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 
 /*----------------------------------------------------------------
-
+// starting at index i no of characters which are same as prefix of the string
 */
 
 
-void z(){
+void z_function(){
     string s;
     cin >> s;
     int n = s.size();
-
     
+    vector<int> z(n,0);
+    for(int i=1, l =0, r =0; i<n; i++){
+        if(i<=r){
+            z[i] = min(z[i-l],r-i+1);
+        }
+
+        while(i+z[i]<n && s[z[i]]==s[i+z[i]]) z[i]++;
+        if(i+z[i]-1>r){
+            l = i;
+            r = i+z[i]-1;
+        }
+    }
+
+    for(int i=0;i<n;i++) cout << z[i] << " ";
  
 }
 
 int32_t main(){
-    z();
+    z_function();
 }
-
-
-
