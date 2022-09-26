03096a53d9ce02ff6b89dbee4b2fc88ae387fbad
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-09-26 11:37:40 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long long double
+#define ar array
+#define mod 1000000007
+#define mod2 998244353
+#define endl '\n'
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define loop(i,m,n) for(int i=m;i<n;i++)
+#define EACH(a, A) for(auto& a: A)
+#define fi first
+#define se second
+#define maxheap priority_queue<ll>
+#define minheap priority_queue<ll,vector<ll>,greater<ll>>
+#define SpeedForce ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0); 
+const ll INF=1e18L;
+const double PI=4*atan(1);
+const double eps=1e-8; 
+const int dx[8]={-1, 0, 0, 1, 1, 1, -1, -1}, dy[8]={0, 1, -1, 0, 1, -1, -1, 1};
+
+void solve(){
+    
+    
+}
+
+int32_t main(){
+    SpeedForce
+    int t = 1;
+    cin >> t;
+    loop(i,0,t){
+        //cout << "Case #" << i+1 << ": ";
+        //cout << fixed << setprecision(6);
+        solve();
+    }
+    //cerr << "Time : " << 1000*((double)clock())/ (double)CLOCKS_PER_SEC << "ms" << endl;
+}
+
+
+
+
+
+
