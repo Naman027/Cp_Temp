3429bf3547e3e382bc5a05905d3c5180ead05432
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-10-07 20:25:33 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long int
@@ -12,37 +12,34 @@ using namespace std;
 #define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
 
-#define loop(i,m,n) for(int i=m;i<n;i++)
-#define EACH(a, A) for(auto& a: A)
+#define loop(i, m, n) for (int i = m; i < n; i++)
+#define EACH(a, A) for (auto &a : A)
 #define fi first
 #define se second
 #define maxheap priority_queue<ll>
-#define minheap priority_queue<ll,vector<ll>,greater<ll>>
-#define SpeedForce ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0); 
-const ll INF=1e18L;
-const double PI=4*atan(1);
-const double eps=1e-8; 
-const int dx[8]={-1, 0, 0, 1, 1, 1, -1, -1}, dy[8]={0, 1, -1, 0, 1, -1, -1, 1};
+#define minheap priority_queue<ll, vector<ll>, greater<ll>>
+#define SpeedForce                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+const ll INF = 1e18L;
+const double PI = 4 * atan(1);
+const double eps = 1e-8;
+const int dx[8] = {-1, 0, 0, 1, 1, 1, -1, -1}, dy[8] = {0, 1, -1, 0, 1, -1, -1, 1};
 
-void solve(){
+void solve(){ 
     
-    
+
 }
 
 int32_t main(){
-    SpeedForce
+    SpeedForce 
     int t = 1;
     cin >> t;
-    loop(i,0,t){
-        //cout << "Case #" << i+1 << ": ";
-        //cout << fixed << setprecision(6);
+    loop(i, 0, t){
+        // cout << "Case #" << i+1 << ": ";
+        // cout << fixed << setprecision(6);
         solve();
     }
-    //cerr << "Time : " << 1000*((double)clock())/ (double)CLOCKS_PER_SEC << "ms" << endl;
+    // cerr << "Time : " << 1000*((double)clock())/ (double)CLOCKS_PER_SEC << "ms" << endl;
 }
-
-
-
-
-
-
