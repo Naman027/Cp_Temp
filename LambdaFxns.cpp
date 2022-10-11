ceeb59c42cfc4f5205629e7a737bd7fa17feefbc
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-10-11 19:42:08 +0530

    Create LambdaFxns.cpp

---
+++ LambdaFxns.cpp
@@ -0,0 +1,12 @@
+// Writing recursive Function 
+function<void(int,int,int)> build = [&](int l,int r,int ind) -> void {
+    if(l==r){
+        Node node;
+        seg[ind] = node;
+        return;
+    }
+    int mid = (l+r)/2;
+    build(l,mid,2*ind+1);
+    build(mid+1,r,2*ind+2);
+    return;
+};
